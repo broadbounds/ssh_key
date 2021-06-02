c96626077454cc6677bf1aece663867c7323f300
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-06-01 19:39:52 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+# We create an ssh key using the RSA algorithm with 4096 rsa bits
+# The ssh key always includes the public and the private key
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# We save our private key at our specified path.
+# Allows private key instead of a password to securely access our instances
+resource "local_file" "save_public_key" {
+  content = tls_private_key.ssh_key.public_key_openssh 
+  filename = "${var.key_path}${var.public_key_name}.pem"
+}
+
+# We save our private key at our specified path.
+# Allows private key instead of a password to securely access our instances
+resource "local_file" "save_public_key" {
+  content = tls_private_key.ssh_key.private_key_pem
+  filename = "${var.key_path}${var.private_key_name}.pem"
+}
